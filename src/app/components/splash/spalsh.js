0c5843d54cb4791e8f6fb588b9db69bacd7e793a
DarkYoung <16302010059@fudan.edu.cn>
2019-05-10 12:46:56 +0800

    新增动画效果

--- src/app/components/splash/spalsh.js
+++ src/app/components/splash/spalsh.js
@@ -1,13 +1,14 @@
 /*
  * @Author: JasonZhang 
  * @Date: 2019-05-10 11:26:45 
- * @Last Modified by:   JasonZhang 
- * @Last Modified time: 2019-05-10 11:26:45 
+ * @Last Modified by: JasonZhang
+ * @Last Modified time: 2019-05-10 12:44:39
  */
 // 加载模块css
 require('./splash.less');
 // 加载html模板
 const html = require('./splash.html');
+
 
 // 导出模块
 module.exports = $(html).clone();
--- src/app/page/index.js
+++ src/app/page/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: JasonZhang 
  * @Date: 2019-05-10 11:27:19 
- * @Last Modified by:   JasonZhang 
- * @Last Modified time: 2019-05-10 11:27:19 
+ * @Last Modified by: JasonZhang
+ * @Last Modified time: 2019-05-10 12:45:37
  */
 // 导入css
 require('../../css/lib/reset.css');
@@ -13,12 +13,22 @@ const Dialog = require('../components/dialog/index');
 const $splash = require('../components/splash/spalsh');
 const Entry = require('../components/entry/entry');
 const entry = new Entry();
+const descText = $($splash).find('#desc_none').html();
+let count = 0;
 
+function setText() {
+  $($splash).find('#desc').html(descText.substring(0, count++));
+  if (count > descText.length)
+    return;
+  else
+    setTimeout(setText, 100);
+}
 // 绑定事件
 $(document).ready(function () {
   // $('.btn').on('click', function () {
   // Dialog();
   // });
   $('#splash').append($splash);
+  setText();
   $('#article').append(entry.getEntries());
 });
