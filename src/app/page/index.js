26728bbc33e4f797255dddacfc76c44d4a0a12a1
DarkYoung <16302010059@fudan.edu.cn>
2019-05-10 13:01:25 +0800

    添加跳转事件

--- src/app/page/index.js
+++ src/app/page/index.js
@@ -2,7 +2,7 @@
  * @Author: JasonZhang 
  * @Date: 2019-05-10 11:27:19 
  * @Last Modified by: JasonZhang
- * @Last Modified time: 2019-05-10 12:45:37
+ * @Last Modified time: 2019-05-10 12:58:32
  */
 // 导入css
 require('../../css/lib/reset.css');
@@ -11,8 +11,7 @@ require('../../css/common/header.less');
 require('../../css/page/index.less');
 const Dialog = require('../components/dialog/index');
 const $splash = require('../components/splash/spalsh');
-const Entry = require('../components/entry/entry');
-const entry = new Entry();
+const entries = require('../components/entry/entry');
 const descText = $($splash).find('#desc_none').html();
 let count = 0;
 
@@ -30,5 +29,5 @@ $(document).ready(function () {
   // });
   $('#splash').append($splash);
   setText();
-  $('#article').append(entry.getEntries());
+  $('#article').append(entries);
 });
